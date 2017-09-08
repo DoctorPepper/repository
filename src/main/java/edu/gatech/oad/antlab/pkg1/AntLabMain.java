081703b0accd056f5ffb89561ab86f0c5a363870
Maddie McRoy <madisonmcroy@lawn-143-215-84-245.lawn.gatech.edu>
2017-09-08 11:38:53 -0400

    resave

--- src/main/java/edu/gatech/oad/antlab/pkg1/AntLabMain.java
+++ src/main/java/edu/gatech/oad/antlab/pkg1/AntLabMain.java
@@ -73,13 +73,8 @@ import edu.gatech.oad.antlab.pkg2.*;
                   toPrint += p4.toString("gburdell4");
                   //Person5 replace P4 with your name
                   //and gburdell5 with your gt id
-<<<<<<< HEAD
-                  Person5 p5 = new Person5("P5");
-                  toPrint += p5.toString("gburdell5");
-=======
                   Person5 p5 = new Person5("Nate Knauf");
                   toPrint += p5.toString("nknauf3");
->>>>>>> 9395f6a9a75c4ccb79061b5562f7bb117efb321e
 
         System.out.println(toPrint);
 
